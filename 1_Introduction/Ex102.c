b744ec10fd74cc35984a36205a2f7d818a7eff95
Gonçalo Heleno <91502223+gheleno-heigvd@users.noreply.github.com>
2022-03-07 16:08:46 +0100

    feat: add more comments and another test
    Added another test demonstrated by the prof, that will test if we have defined B with correct parenthesis.

--- 1_Introduction/Ex102.c
+++ 1_Introduction/Ex102.c
@@ -25,11 +25,14 @@ Compilator : Mingw-w64 g++ 11.2.0
 #include <stdlib.h>
 
 #define A 2
-#define B (A + 1)
-#define PLUS(X, Y) ((X) + (Y))
+#define B ((A) + 1)   // We could have A without the parenthesis, since we ONLY have
+                      // additions and subtractions
+#define PLUS(X, Y) ((X) + (Y))    // If we had a blank space here as before, we
+                                  // would replace PLUS for everything after
 #define MOINS(X, Y) ((X) - (Y))
 
 int main(void) {
    printf("%d %d\n", 5 * PLUS(A, B), MOINS(A + 1, B + 1));
+   printf("%d\n", A * B);
    return EXIT_SUCCESS;
 }
